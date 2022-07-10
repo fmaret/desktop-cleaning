2735f2ce99511f0f9693b48b303610720334e41a
Fassi Théo <gtheya@gmail.com>
2022-07-10 16:08:55 +0200

    v1/2

--- main.py
+++ main.py
@@ -1,10 +1,9 @@
 #!/usr/bin/env python
 
-import msvcrt
 import sys
 import time
 import os
-import random
+import re
 
 import commctrl as cc
 import win32gui as wgui
@@ -17,6 +16,27 @@ class Folders(Enum):
   GAMES: str = "Games"
   APPS: str = "Apps"
   DOCUMENTS: str = "Documents"
+  LAUNCHERS: str = "Launchers"
+
+class Extension(Enum):
+  URL = "url"
+  LNK = "lnk"
+  EXE = "exe"
+
+def readLinesTxt(path: str):
+  lines = []
+  for line in open(path):
+    if line.startswith("#"):
+      continue
+
+    lines.append(line.strip())
+
+    if not line:
+      continue
+    
+  return lines 
+
+
   
 def getWnd():
   search_criteria = (
@@ -40,41 +60,67 @@ def moveTo(x: int, y: int, index: int):
 
   wgui.SendMessage(wnd, cc.LVM_SETITEMPOSITION, index, lparam)
 
+apps = readLinesTxt("./apps.txt")
+launchers = readLinesTxt("./launchers.txt")
+
+def isInList(file: str, list_: list[str]):
+  file = ".".join(file.split(".")[:-1])
+
+  print(file)
+  
+  for item in list_:
+    regex = r"\b(" + item.lower() + r")\b"
+    if bool(re.match(regex, file.lower().replace(u'\xa0', u' '))):
+      return True
+
+  return False
+
+
+def isGame(file: str):
+  return file.endswith(".url") or (file.endswith(".lnk") and not isInList(file, apps) and not isInList(file, launchers))
+  
+def isApp(file: str):
+  return file.endswith(".lnk")
+
+def isLauncher(file: str):
+  return (file.endswith(".lnk") and isInList(file, launchers))
+  
+def getType(file):
+  if isGame(file):
+    return Folders.GAMES.value
+  elif isLauncher(file):
+    return Folders.LAUNCHERS.value
+  elif isApp(file):
+    return Folders.APPS.value
+  elif file.split(".")[1]:
+      return Folders.DOCUMENTS.value
+
 def sortFiles():
   files = os.listdir(DESKTOP_PATH)
 
-  for folder in Folders:
+  for folder in Folders:  
     # Create folder if it doesn't exist
     if not os.path.exists(DESKTOP_PATH + "/" + folder.value):
       os.makedirs(DESKTOP_PATH + "/" + folder.value)
 
-
-  for file in [file for file in files if len(file.split(".")) == 2]: 
-    if file.endswith(".url"):
-      # Move it to the folder Games
-      os.rename(DESKTOP_PATH + "/" + file, DESKTOP_PATH + "/" +Folders.GAMES.value +"/"+ file)
-    elif file.endswith(".exe"):
-      # Move it to the folder Apps
-      os.rename(DESKTOP_PATH + "/" + file, DESKTOP_PATH + "/" +Folders.APPS.value +"/" + file)
-    elif file.endswith(".lnk"):
-      # Check if file is a game
-      os.rename(DESKTOP_PATH + "/" + file, DESKTOP_PATH + "/" +Folders.APPS.value +"/" + file)
-    elif file.split(".")[1]:
-      # Move it to the folder Apps
-      os.rename(DESKTOP_PATH + "/" + file, DESKTOP_PATH + "/" +Folders.DOCUMENTS.value +"/" + file)
-  
+  print(files)
+  for file in [file for file in files if len(file.split(".")) > 1]: 
+    print(file)
+    os.rename(DESKTOP_PATH + "/" + file, DESKTOP_PATH + "/" + getType(file) +"/"+ file)
   return 
   
 def main(*argv):
     wnd = getWnd()
     idx = 1
-    wgui.SendMessage(wnd, cc.LVM_UPDATE, idx, 0)
 
     sortFiles()
 
+    wgui.SendMessage(wnd, cc.LVM_UPDATE, idx, 0)
 
     x = 1920
     y = 1080
+
+    moveTo(x, y, 1)
     
     time.sleep(0.1)
 
